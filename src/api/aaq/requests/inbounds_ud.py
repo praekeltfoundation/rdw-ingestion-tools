b3a19e17bb12a0394eaf3b07662fdbd31eca20a3
Schalk1e <schalk@praekelt.org>
2023-01-25 10:30:42 +0200

    Adding urgency detection things.

---
+++ src/api/aaq/requests/inbounds_ud.py
@@ -0,0 +1,28 @@
+from pandas import DataFrame, concat
+
+
+class InboundsUD:
+    def __init__(self, session) -> None:
+        self._session = session
+
+    def get_inbounds_ud(self, **kwargs):
+
+        url = "inbounds_ud"
+
+        response_list = self._session.get(url, **kwargs)
+
+        response_list = [
+            {key: str(d[key]) for key in d.keys()} for d in response_list
+        ]
+
+        try:
+            inbounds_ud = concat(
+                [DataFrame(d, index=[0]) for d in response_list]
+            )
+        except ValueError as e:
+            if str(e) != "No objects to concatenate":
+                raise
+            else:
+                inbounds_ud = DataFrame()
+
+        return inbounds_ud
---
+++ src/api/aaq/requests/urgency_rules.py
@@ -0,0 +1,28 @@
+from pandas import DataFrame, concat
+
+
+class UrgencyRules:
+    def __init__(self, session) -> None:
+        self._session = session
+
+    def get_urgency_rules(self, **kwargs):
+
+        url = "urgency_rules"
+
+        response_list = self._session.get(url, **kwargs)
+
+        response_list = [
+            {key: str(d[key]) for key in d.keys()} for d in response_list
+        ]
+
+        try:
+            urgency_rules = concat(
+                [DataFrame(d, index=[0]) for d in response_list]
+            )
+        except ValueError as e:
+            if str(e) != "No objects to concatenate":
+                raise
+            else:
+                urgency_rules = DataFrame()
+
+        return urgency_rules
