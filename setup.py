ce5e0455c42acd4bc21ab738aab0fa696177305d
Schalk1e <schalk@praekelt.org>
2023-05-13 12:58:52 +0200

    Prep for v0.3.3 (@Schalk1e)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rdw-ingestion-tools",
-    version="0.3.2",
+    version="0.3.3",
     license="MIT",
     description="Utilities for data ingestion.",
     author="Praekelt.org DS Team",
