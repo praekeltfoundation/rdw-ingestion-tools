3edf8479565fb50ec37fbef40c78b70a95dc4d20
Schalk1e <sv973005@gmail.com>
2024-06-06 15:07:05 +0200

    Be consistent abt keywords.

--- src/api/survey/requests/mqr.py
+++ src/api/survey/requests/mqr.py
@@ -22,7 +22,7 @@ class MQR:
         url = "mqrbaselinesurvey/"
 
         baseline_generator = get_paginated(
-            self.client, url=url, start=ts, max_pages=max_pages
+            client=self.client, url=url, start=ts, max_pages=max_pages
         )
 
         baseline_responses = []
