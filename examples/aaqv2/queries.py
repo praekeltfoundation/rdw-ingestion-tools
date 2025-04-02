9f332e8b76eca0f86bf8f4341dd36831f6cce565
Schalk <sv973005@gmail.com>
2025-04-02 13:21:15 +0200

    Add start and end times.

--- examples/aaqv2/queries.py
+++ examples/aaqv2/queries.py
@@ -1,5 +1,8 @@
 from api.aaqv2 import pyAAQV2
 
-queries = pyAAQV2.queries.get_queries()
+start_date = "2025-04-01T00:00:00"
+end_date = "2025-04-02T00:00:00"
+
+queries = pyAAQV2.queries.get_queries(start_date=start_date, end_date=end_date)
 
 queries.head(5)
