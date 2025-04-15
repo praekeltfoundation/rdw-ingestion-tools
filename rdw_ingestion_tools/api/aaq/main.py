f5fadf8f5e93152d910b524c8d9d914340829667
Schalk <sv973005@gmail.com>
2025-04-15 13:41:47 +0200

    Change initialisation for aaq submodule.

--- rdw_ingestion_tools/api/aaq/main.py
+++ rdw_ingestion_tools/api/aaq/main.py
@@ -1,15 +1,31 @@
+from attrs import define, field
+from httpx import Client
+
 from api.aaq.requests.faqmatches import FAQMatches
 from api.aaq.requests.inbounds import Inbounds
 from api.aaq.requests.inbounds_ud import InboundsUD
 from api.aaq.requests.urgency_rules import UrgencyRules
 
-from . import client
+from . import client as default_client
 
 
+@define
 class pyAAQ:
-    """A wrapper class for the various AAQ endpoints."""
+    """A wrapper class for the various AAQ endpoints.
 
-    inbounds = Inbounds(client)
-    faqmatches = FAQMatches(client)
-    inbounds_ud = InboundsUD(client)
-    urgency_rules = UrgencyRules(client)
+    The client is configurable to it can be switched out in tests.
+
+    """
+
+    client: Client = field(factory=lambda: default_client)
+
+    inbounds = field(init=False)
+    faqmatches = field(init=False)
+    inbounds_ud = field(init=False)
+    urgency_rules = field(init=False)
+
+    def __attrs_post_init__(self):
+        self.inbounds = Inbounds(self.client)
+        self.faqmatches = FAQMatches(self.client)
+        self.inbounds_ud = InboundsUD(self.client)
+        self.urgency_rules = UrgencyRules(self.client)
