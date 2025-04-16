55a86fba4843796a3b4f5381965a2c15c45d3b12
Schalk <sv973005@gmail.com>
2025-04-16 09:47:50 +0200

    Change initialisation for flow_results submodule.

--- rdw_ingestion_tools/api/flow_results/main.py
+++ rdw_ingestion_tools/api/flow_results/main.py
@@ -1,11 +1,25 @@
 from api.flow_results.requests.flows import Flows
 from api.flow_results.requests.responses import Responses
 
-from . import client
+from . import client as default_client
 
+from httpx import Client
 
+from attrs import define, field
+
+@define
 class pyFlows:
-    """A wrapper class for the various Flow Results endpoints."""
+    """A wrapper class for the various Flow Results endpoints.
 
-    flows = Flows(client)
-    responses = Responses(client)
+    The client is configurable so it can be switched out in tests.
+
+    """
+
+    client: Client = field(factory=lambda: default_client)
+
+    flows = field(init=False)
+    responses = field(init=False)
+
+    def __attrs_post_init__(self):
+        self.flows = Flows(client=self.client)
+        self.responses = Responses(client=self.client)
