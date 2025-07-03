a93e11da5a12e963594eeda80610be4f98cd52ab
TuscanyBothaReach <tuscany@reachdigitalhealth.org>
2025-07-03 10:01:44 +0200

    Typo

--- examples/turn_bq/chats.py
+++ examples/turn_bq/chats.py
@@ -2,7 +2,7 @@ from api.turn_bq import pyTurnBQ
 
 chats = pyTurnBQ().chats.get_chats_by_id(chat_id=1531207597)
 
-print(chats..collect().head(5))
+print(chats.collect().head(5))
 
 chats = pyTurnBQ().chats.get_chats_by_updated_at(
     from_timestamp="2023-10-01T00:00:00", to_timestamp="2023-10-01T00:50:00"
