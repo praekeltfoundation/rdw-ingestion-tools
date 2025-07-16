522065effa8460fb81ad849a3e75e44b12a061e8
TuscanyBothaReach <tuscany@reachdigitalhealth.org>
2025-07-16 09:31:10 +0200

    Add schema tests

--- tests/test_aaqv2.py
+++ tests/test_aaqv2.py
@@ -1,7 +1,15 @@
+import typing
+
 import pytest
 from api.aaqv2 import pyAAQV2
 from polars import LazyFrame
+from polars.exceptions import SchemaError
 from polars.testing import assert_frame_equal
+
+from rdw_ingestion_tools.api.aaqv2.schemas.contents import contents_schema
+from rdw_ingestion_tools.api.aaqv2.schemas.queries import queries_schema
+from rdw_ingestion_tools.api.aaqv2.schemas.urgency_queries import urgency_queries_schema
+from rdw_ingestion_tools.api.aaqv2.schemas.urgency_rules import urgency_rules_schema
 
 from .fake_aaqv2.api import FakeAAQV2
 from .fake_aaqv2.models import (
@@ -245,3 +253,256 @@ def test_get_urgency_queries_single_record_outside_timeframe(fake_aaqv2, aaqv2):
     )
 
     assert urgency_queries.collect().is_empty()
+
+
+@pytest.mark.xfail(
+    reason="Not implemented yet: should raise an error instead of dropping extra fields"
+)
+def test_urgency_queries_with_extra_struct_field(fake_aaqv2, aaqv2):
+    """
+    Tests that an extra struct in the urgency queries response details causes an error.
+    This is expected to fail as Polars silently drops unexpected struct fields.
+    We would prefer these unexpected fields to cause pipeline failures.
+    """
+    response_details = UrgencyQueryResponseDetails(distance=0.05, urgency_rule="rule")
+
+    # Adds an extra response detail struct to the details field.
+    # This should fail as the existing schema has 100 fields.
+    response = UrgencyQueryResponseExtract(
+        details={str(i): response_details for i in range(101)},
+        is_urgent=True,
+        matched_rules=["rule"],
+        response_datetime_utc="2020-01-01T01:00:00",
+        urgency_response_id=1,
+    )
+
+    urgency_query = UrgencyQuery(
+        message_datetime_utc="2020-01-01T01:00:00",
+        message_text="Urgent text!",
+        response=response,
+        urgency_query_id=1,
+        workspace_id=0,
+    )
+
+    fake_aaqv2.add_urgency_queries(urgency_query)
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+
+    with pytest.raises(SchemaError):
+        aaqv2.urgency_queries.get_urgency_queries(
+            start_date=start_date, end_date=end_date
+        )
+
+
+@pytest.mark.xfail(
+    reason="Not implemented yet: should raise an error instead of dropping extra fields"
+)
+def test_queries_fails_with_extra_struct_field(fake_aaqv2, aaqv2):
+    """
+    Tests that an extra field in search_results will cause an error.
+    This is expected to fail as Polars silently drops unexpected struct fields.
+    We would prefer these unexpected fields to cause pipeline failures.
+    """
+    search_results = SearchResults(title="Title", text="text", id=0, distance=0.05)
+
+    query_response = QueryResponse(
+        response_id=1,
+        search_results={
+            "0": search_results,
+            "1": search_results,
+            "2": search_results,
+            "3": {
+                "title": "Title",
+                "text": "text",
+                "id": 0,
+                "distance": 0.05,
+                "unexpected_field": True,  # type: ignore
+            },
+        },
+        llm_response="response",
+        response_datetime_utc="2020-01-01T00:00:00",
+    )
+
+    response_feedback = ResponseFeedback(
+        feedback_id=1,
+        feedback_sentiment="Bad",
+        feedback_text=None,
+        feedback_datetime_utc="2020-01-01T00:00:00",
+    )
+
+    content_feedback = ContentFeedback(
+        feedback_id=1,
+        feedback_sentiment="Bad",
+        feedback_text="Bad",
+        feedback_datetime_utc="2020-01-01T00:00:00",
+        content_id=1,
+    )
+
+    query = Query(
+        query_id=1,
+        query_text="This is my query.",
+        query_metadata="Some metadata",
+        query_datetime_utc="2020-01-01T00:00:00",
+        response=[query_response],
+        response_feedback=[response_feedback],
+        content_feedback=[content_feedback],
+        workspace_id=0,
+    )
+
+    fake_aaqv2.add_queries(query)
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+
+    with pytest.raises(SchemaError):
+        aaqv2.queries.get_queries(start_date=start_date, end_date=end_date)
+
+
+@typing.no_type_check
+def test_get_contents_schema_null_integrity(fake_aaqv2, aaqv2):
+    """
+    Test that the contents schema is maintained when fields have no value
+
+    """
+    content = Content(
+        content_title=None,
+        content_text=None,
+        content_tags=None,
+        content_metadata=None,
+        is_archived=None,
+        content_id=None,
+        display_number=None,
+        created_datetime_utc=None,
+        negative_votes=None,
+        positive_votes=None,
+        updated_datetime_utc=None,
+        workspace_id=None,
+    )
+
+    fake_aaqv2.add_contents(content)
+
+    contents = aaqv2.contents.get_contents()
+
+    assert contents.collect_schema() == contents_schema
+
+
+@typing.no_type_check
+def test_get_urgency_rules_schema_null_integrity(fake_aaqv2, aaqv2):
+    """
+    Test that the urgency rules schema is maintained when fields have no value
+
+    """
+    urgency_rule = UrgencyRule(
+        urgency_rule_metadata=None,
+        urgency_rule_text=None,
+        created_datetime_utc=None,
+        updated_datetime_utc=None,
+        urgency_rule_id=None,
+        workspace_id=None,
+    )
+
+    fake_aaqv2.add_urgency_rules(urgency_rule)
+
+    urgency_rules = aaqv2.urgency_rules.get_urgency_rules()
+
+    assert urgency_rules.collect_schema() == urgency_rules_schema
+
+
+@typing.no_type_check
+def test_get_urgency_queries_schema_null_integrity(fake_aaqv2, aaqv2):
+    """
+    Test that the urgency queries schema is maintained when fields have no value
+
+    """
+    urgency_query = UrgencyQuery(
+        message_datetime_utc="2020-01-01T00:00:00",
+        message_text=None,
+        response=None,
+        urgency_query_id=None,
+        workspace_id=None,
+    )
+    fake_aaqv2.add_urgency_queries(urgency_query)
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+    urgency_queries = aaqv2.urgency_queries.get_urgency_queries(
+        start_date=start_date, end_date=end_date
+    )
+
+    assert urgency_queries.collect_schema() == urgency_queries_schema
+
+
+@typing.no_type_check
+def test_get_queries_schema_null_integrity(fake_aaqv2, aaqv2):
+    """
+    Test that the queries schema is maintained when fields have no value
+
+    """
+    query = Query(
+        query_id=None,
+        query_text=None,
+        query_metadata=None,
+        query_datetime_utc="2020-01-01T00:00:00",
+        response=None,
+        response_feedback=None,
+        content_feedback=None,
+        workspace_id=None,
+    )
+
+    fake_aaqv2.add_queries(query)
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+    queries = aaqv2.queries.get_queries(start_date=start_date, end_date=end_date)
+
+    assert queries.collect_schema() == queries_schema
+
+
+def test_get_contents_schema_empty_response(aaqv2):
+    """
+    Test that the contents schema is maintained when the response is empty
+
+    """
+
+    contents = aaqv2.contents.get_contents()
+
+    assert contents.collect_schema() == contents_schema
+
+
+def test_get_urgency_rules_schema_empty_response(aaqv2):
+    """
+    Test that the urgency rules schema is maintained when fields have no value
+
+    """
+    urgency_rules = aaqv2.urgency_rules.get_urgency_rules()
+
+    assert urgency_rules.collect_schema() == urgency_rules_schema
+
+
+def test_get_urgency_queries_schema_empty_response(aaqv2):
+    """
+    Test that the urgency queries schema is maintained when fields have no value
+
+    """
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+    urgency_queries = aaqv2.urgency_queries.get_urgency_queries(
+        start_date=start_date, end_date=end_date
+    )
+
+    assert urgency_queries.collect_schema() == urgency_queries_schema
+
+
+def test_get_queries_schema_empty_response(aaqv2):
+    """
+    Test that the queries schema is maintained when fields have no value
+
+    """
+
+    start_date = "2020-01-01T00:00:00"
+    end_date = "2020-01-02T00:00:00"
+    queries = aaqv2.queries.get_queries(start_date=start_date, end_date=end_date)
+
+    assert queries.collect_schema() == queries_schema
